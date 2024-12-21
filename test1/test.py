0b58db40942e8afa8e31f43c5a395528c2b885a9
Leonid181099 <79803160+Leonid181099@users.noreply.github.com>
2024-12-21 12:53:13 +0300

    Update test.py

--- test1/test.py
+++ test1/test.py
@@ -2,7 +2,7 @@ import http.client
 import re
 conn = http.client.HTTPConnection('127.0.0.1',5000)
 post=input()
-if post=='':
+if post=='test':
     post=field_name_1='field_name_1=abc@mail.lol&field_name_2=+7 999 999 99 99&field_name_3=+7 999 999 99 99'
 post=re.sub(r'\s','%20',post)
 conn.request("POST", "get_form?"+post)
